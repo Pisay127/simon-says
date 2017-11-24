7f513c7b61c5f0adbc602d13112681837cf4e6cd
Nikka <dejesusnikka@gmail.com>
2017-11-24 11:25:30 +0800

    Done welcome screen(partial)

--- game.asm
+++ game.asm
@@ -1,7 +1,65 @@
 .model small
 .stack 100h
 .data
+	welcomeMsg db "SIMON SAYS!", 13, 10, "$"
 .code
     main proc
-    endp
+		mov ax, @data
+		mov ds, ax
+		mov ax, 0
+		
+		mov ah, 06
+		mov al, 00
+		mov bh, 00
+		mov ch, 00
+		mov cl, 00
+		mov dh, 24
+		mov dl, 79
+		int 10h
+		
+		mov ah, 06
+		mov al, 00
+		mov bh, 40
+		mov ch, 00
+		mov cl, 00
+		mov dh, 24
+		mov dl, 79
+		int 10h
+		
+		mov ah, 06
+		mov al, 00
+		mov bh, 50
+		mov ch, 00
+		mov cl, 00
+		mov dh, 12
+		mov dl, 79
+		int 10h
+		
+		mov ah, 06
+		mov al, 00
+		mov bh, 30
+		mov ch, 00
+		mov cl, 00
+		mov dh, 24
+		mov dl, 39
+		int 10h
+		
+		mov ah, 06
+		mov al, 00
+		mov bh, 64
+		mov ch, 00
+		mov cl, 00
+		mov dh, 12
+		mov dl, 39
+		int 10h
+		
+		int 33h
+		mov dx, offset welcomeMsg
+		mov ah, 09h
+		int 21h
+		
+		mov ah, 4ch
+		int 21h
+		
+    main endp
 end main
